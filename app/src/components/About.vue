eb67aa01e0d65609667ae5b158d54d9352a390cb
JasonBai007 <byygkcg@163.com>
2019-06-25 10:16:50 +0800

    修改动画

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1 class="animated fadeInUp">蒜瓣</h1>
-    <h2 class="animated flipInX">大蒜的蒜</h2>
-    <h2 class="animated flipInX">豆瓣的瓣</h2>
+    <h2 class="animated fadeInUp" style="animation-delay:0.1s">大蒜的蒜</h2>
+    <h2 class="animated fadeInUp" style="animation-delay:0.2s">豆瓣的瓣</h2>
     <a href="https://github.com/JasonBai007" target="_blank">
       <i class="fa fa-github-square"></i>
     </a>
--- app/src/components/Loading.vue
+++ app/src/components/Loading.vue
@@ -1,59 +1,75 @@
 <template>
-  <div class="loading"  v-show="isOpen">
-    <div class="spinner"></div>
+  <div class="loading" v-show="isOpen">
+    <div class="balls">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'loading',
-  props:['isOpen'],
-  data () {
+  name: "loading",
+  props: ["isOpen"],
+  data() {
     return {
       isLoading: true
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 .loading {
   position: fixed;
-  top:0;
+  top: 0;
   bottom: 0;
-  left:0;
-  right:0;
-  background: rgba(255,255,255,0.6);
+  left: 0;
+  right: 0;
+  background: rgba(255, 255, 255, 0.6);
   display: flex;
   justify-content: center;
-  align-items:center;
+  align-items: center;
 }
-.spinner {
-  width: 3rem;
-  height: 3rem;
-  background-color: #1ABC9C;
-  opacity: 1;
-  z-index: 10;
-  -webkit-animation: rotateplane 1.2s infinite ease-in-out;
-  animation: rotateplane 1.2s infinite ease-in-out;
+.balls {
+  width: 4em;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
 }
 
-@-webkit-keyframes rotateplane {
-  0% { -webkit-transform: perspective(120px) }
-  50% { -webkit-transform: perspective(120px) rotateY(180deg) }
-  100% { -webkit-transform: perspective(120px) rotateY(180deg)  rotateX(180deg) }
+.balls div {
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: 50%;
+  background-color: #1abc9c;
 }
 
-@keyframes rotateplane {
-  0% {
-    transform: perspective(120px) rotateX(0deg) rotateY(0deg);
-    -webkit-transform: perspective(120px) rotateX(0deg) rotateY(0deg)
-  } 50% {
-    transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg);
-    -webkit-transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg)
-  } 100% {
-    transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
-    -webkit-transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
+.balls div:nth-of-type(1) {
+  transform: translateX(-100%);
+  animation: left-swing 0.45s ease-in alternate infinite;
+}
+
+.balls div:nth-of-type(3) {
+  transform: translateX(-95%);
+  animation: right-swing 0.45s ease-out alternate infinite;
+}
+
+@keyframes left-swing {
+  50%,
+  100% {
+    transform: translateX(95%);
+  }
+}
+
+@keyframes right-swing {
+  50% {
+    transform: translateX(-95%);
+  }
+  100% {
+    transform: translateX(100%);
   }
 }
 </style>
